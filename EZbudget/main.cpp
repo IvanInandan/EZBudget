a6c5f12c02712b001684bde1af23419e14931f94
Elaine Joy Mutuc <ejmutuc@gmail.com>
2018-03-21 13:04:47 -0700

    include testsignal.

--- EZbudget/main.cpp
+++ EZbudget/main.cpp
@@ -3,8 +3,9 @@
 #include <QObject>
 #include <iostream>
 #include "account.h"
+<<<<<<< HEAD
 #include "loginscreen.h"
-//#include "testsignal.h"
+#include "testsignal.h"
 =======
 >>>>>>> 9f569f7e4cae5f6ba88cb02b8616dd216728345a
 using namespace std;
